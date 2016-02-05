3d031803d671482d7df24afc3ce193a90bd8c92c
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2016-02-05 00:32:44 -0500

    Fix typo.

--- include/engine/intdef.h
+++ include/engine/intdef.h
@@ -8,9 +8,9 @@ namespace nge
 {
 
 // INTEGERS
-typedef uint8_t int8;
-typedef uint16_t int16;
-typedef uint32_t int32;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
 typedef int64_t int64;
 
 // UNSIGNED INTEGERS
