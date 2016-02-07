d74f66e0dbb57687345e234d8ba7d88f61ee7196
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2016-02-07 15:24:30 -0500

    Fix styling issues and minor bugs.

--- include/engine/memory/counting_allocator.h
+++ include/engine/memory/counting_allocator.h
@@ -1,8 +1,11 @@
 // counting_allocator.h
-#ifndef NGE_MEM_DEFAULT_ALLOCATOR_H
-#define NGE_MEM_DEFAULT_ALLOCATOR_H
+#ifndef NGE_MEM_COUNTING_ALLOCATOR_H
+#define NGE_MEM_COUNTING_ALLOCATOR_H
 
 #include <assert.h>
+
+#include "engine/intdef.h"
+#include "engine/memory/allocator_guard.h"
 
 namespace nge
 {
@@ -10,43 +13,59 @@ namespace nge
 namespace mem
 {
 
-template<typename T>
-class CountingAllocator : DefaultAllocator<T>
+template <typename T>
+class CountingAllocator : public IAllocator<T>
 {
   private:
-    // DATA MEMBERS
+    // GLOBALS
     /**
-    * Count of globally allocated instances of class T.
-    */
-    static uint32 _gblAllocCount;
+     * Global instance count of type T.
+     */
+    static uint32 g_count;
+
+    // MEMBERS
+    /**
+     * The underlying allocator.
+     */
+    AllocatorGuard<T> _allocator;
 
     /**
-    * Count of locally allocated instances of class T.
-    */
-    uint32 lclAllocCount;
+     * Local instance count of type T.
+     */
+    uint32 _count;
 
   public:
     // CONSTRUCTORS
     /**
-    * Constructs the counting allocator.
-    */
+     * Constructs the counting allocator using the default allocator.
+     */
     CountingAllocator();
 
     /**
-    * Counstructs a copy of a counting allocator.
-    */
-    CountingAllocator( const CountingAllocator<T>& copy );
+     * Constructs a counting allocator that performs allocation using the
+     * given allocator.
+     */
+    CountingAllocator( IAllocator<T>* alloc );
 
     /**
-    * Destructs the counting allocator
-    */
+     * Constructs a copy of a counting allocator.
+     *
+     * This does not retain the local allocation count.
+     */
+    CountingAllocator( const CountingAllocator<T>& alloc );
+
+    /**
+     * Destructs the counting allocator
+     */
     virtual ~CountingAllocator();
 
     // OPERATORS
     /**
-    * Assigns a copy of the allocator
-    */
-    virtual CountingAllocator<T> operator =( const CountingAllocator<T>& assign );
+     * Assigns this as a copy of the allocator.
+     *
+     * This does not retain the local allocation count.
+     */
+    CountingAllocator<T>& operator=( const CountingAllocator<T>& alloc );
 
     // MEMBER FUNCTIONS
     /**
@@ -56,104 +75,123 @@ class CountingAllocator : DefaultAllocator<T>
      * T is void
      * count is less than or equal to zero
      * out of memory
-    */
+     */
      virtual T* get( uint32 count );
 
      /**
-     * Releases the allocation with the given number of instances.
-     *
-     * Behavior is undefined when:
-     * T is void
-     * pointer is invalid
-     * count is less than or equal to zero
-     * out of memory
-     */
+      * Releases the allocation with the given number of instances.
+      *
+      * Behavior is undefined when:
+      * T is void
+      * pointer is invalid
+      * count is less than or equal to zero
+      * out of memory
+      */
      virtual void release( T* pointer, uint32 count );
 
      // ACCESSOR FUNCTIONS
      /**
-     * Gets the local allocation count
-     */
-     uint32 getLocalAllocationCount();
+      * Gets the number of instance of T that are currently allocated locally.
+      */
+     uint32 getAllocationCount();
 
-     // GLOBAL ACCESSOR FUNCTIONS
+     // GLOBAL FUNCTIONS
      /**
-     * Gets the global allocation count.
-     */
+      * Gets the global allocation count.
+      */
      static uint32 getGlobalAllocationCount();
-}
+};
 
-// GLOBAL DATA MEMBERS
-template<typename T>
-int CountingAllocator<T>::_gblAllocCount = 0;
+// GLOBALS
+template <typename T>
+uint32 CountingAllocator<T>::g_count = 0;
 
 // CONSTRUCTORS
-template<typename T>
+template <typename T>
 inline
-CountingAllocator<T>::CountingAllocator()
+CountingAllocator<T>::CountingAllocator() : _allocator( nullptr ), _count( 0 )
 {
-    lclAllocCount = 0;
 }
 
-template<typename T>
+template <typename T>
 inline
-CountingAllocator<T>::CountingAllocator( const CountingAllocator<T>& copy )
+CountingAllocator<T>::CountingAllocator( IAllocator<T>* alloc )
+    : _allocator( alloc ), _count( 0 )
 {
-    lclAllocCount = 0;
+}
+
+template <typename T>
+inline
+CountingAllocator<T>::CountingAllocator( const CountingAllocator<T>& alloc )
+    : _allocator( alloc._allocator ), _count( 0 )
+{
+}
+
+template <typename T>
+inline
+CountingAllocator<T>::~CountingAllocator()
+{
+    // check for memory leak
+    assert( _count <= 0 );
+    _allocator = nullptr;
 }
 
 // OPERATORS
-template<typename T>
+template <typename T>
 inline
-CountingAllocator<T>& CountingAllocator<T>::operator =( const CountingAllocator<T>& copy )
+CountingAllocator<T>& CountingAllocator<T>::operator=(
+    const CountingAllocator<T>& copy )
 {
+    _allocator = copy._allocator;
+
     return *this;
 }
 
 // MEMBER FUNCTIONS
-template<typename T>
+template <typename T>
 inline
 T* CountingAllocator<T>::get( uint32 count )
 {
     assert( count > 0 );
 
-    lclAllocCount += count;
-    _gblAllocCount += count;
+    _count += count;
+    g_count += count;
 
     return new T[count];
 }
 
-template<typename T>
+template <typename T>
 inline
 void CountingAllocator<T>::release(T* pointer, uint32 count)
 {
     assert( pointer != nullptr );
     assert( count > 0 );
+    assert( _count >= count );
 
-    lclAllocCount -= count;
-    _gblAllocCount -= count;
+    _count -= count;
+    g_count -= count;
 
-    delete[] ptr;
+    delete[] pointer;
 }
 
 // ACCESSOR FUNCTIONS
-template<typename T>
+template <typename T>
 inline
-uint32 CountingAllocator<T>::getLocalAllocationCount()
+uint32 CountingAllocator<T>::getAllocationCount()
 {
-    return lclAllocCount;
+    return _count;
 }
 
 // GLOBAL ACCESSOR FUNCTIONS
-template<typename T>
+template <typename T>
 inline
 uint32 CountingAllocator<T>::getGlobalAllocationCount()
 {
-    return _gblAllocCount;
+    return g_count;
 }
 
-} //End nmspc mem
+} //End nspc mem
 
-} //End nmspc nge
+} //End nspc nge
 
 #endif
--- test/engine/memory/counting_allocator.t.cpp
+++ test/engine/memory/counting_allocator.t.cpp
@@ -5,6 +5,7 @@
 TEST( CountingAllocator, Construction )
 {
     using namespace nge::mem;
+    using namespace nge;
 
     EXPECT_NO_FATAL_FAILURE(
         CountingAllocator<uint32> alloc;
@@ -15,33 +16,35 @@ TEST( CountingAllocator, Construction )
 TEST( CountingAllocator, Allocation )
 {
     using namespace nge::mem;
+    using namespace nge;
 
     uint32* value = nullptr;
 
     CountingAllocator<uint32> alloc;
     EXPECT_NO_FATAL_FAILURE( value = alloc.get( 1 ) );
-    EXPECT_EQ( alloc.getLocalAllocationCount(), 1 );
-    EXPECT_EQ( CountingAllocator<uint32>.getGlobalAllocationCount(), 1 );
+    EXPECT_EQ( alloc.getAllocationCount(), 1 );
+    EXPECT_EQ( CountingAllocator<uint32>::getGlobalAllocationCount(), 1 );
     EXPECT_NE( nullptr, value );
     EXPECT_NO_FATAL_FAILURE( alloc.release( value, 1 ) );
 
+    EXPECT_DEATH( alloc.release( value, 1 ), ".*" );
     EXPECT_DEATH( alloc.release( value, 0 ), ".*" );
     EXPECT_DEATH( alloc.release( nullptr, 1 ), ".*" );
 
     value = nullptr;
 
     EXPECT_NO_FATAL_FAILURE( value = alloc.get( 100 ) );
-    EXPECT_EQ( alloc.getLocalAllocationCount(), 100 );
-    EXPECT_EQ( CountingAllocator<uint32>.getGlobalAllocationCount(), 100 );
+    EXPECT_EQ( alloc.getAllocationCount(), 100 );
+    EXPECT_EQ( CountingAllocator<uint32>::getGlobalAllocationCount(), 100 );
     EXPECT_NE( nullptr, value );
 
     uint32* otherValue = nullptr;
 
     CountingAllocator<uint32> otherAlloc;
-    EXPECT_NO_FATAL_FAILURE( otherValue = alloc.get( 5 ) );
-    EXPECT_EQ( otherAlloc.getLocalAllocationCount(), 5 );
-    EXPECT_EQ( CountingAllocator<uint32>.getGlobalAllocationCount(), 100 + 5 );
+    EXPECT_NO_FATAL_FAILURE( otherValue = otherAlloc.get( 5 ) );
+    EXPECT_EQ( otherAlloc.getAllocationCount(), 5 );
+    EXPECT_EQ( CountingAllocator<uint32>::getGlobalAllocationCount(), 105 );
     EXPECT_NO_FATAL_FAILURE( alloc.release( value, 100 ) );
-    EXPECT_EQ( CountingAllocator<uint32>.getGlobalAllocationCount(), 5 );
+    EXPECT_EQ( CountingAllocator<uint32>::getGlobalAllocationCount(), 5 );
     EXPECT_NO_FATAL_FAILURE( otherAlloc.release( otherValue, 5 ) );
 }
